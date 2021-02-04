247974d38e6b46f76fa460fa4dd8861ab03b728b
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2021-02-04 14:01:24 +0530

    Added key prop

--- src/pages/DonarDetails.js
+++ src/pages/DonarDetails.js
@@ -1,3 +1,4 @@
+import { uniqueId } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import FullHeading from "../components/FullHeading"
 import Waypoint from '../components/Waypoint';
@@ -24,15 +25,15 @@ function DonarDetails({ element }) {
             <FullHeading title="Donar Details" />
             <Waypoint element={element} />
             <div>
-                <table class="table table-hover" style={{ backgroundColor: "#39bb39" }}>
+                <table class="table table-striped" style={{ backgroundColor: "#39bb39" }}>
                     <thead>
                         <tr>
-                            {tableHead.map((th) => <th className="text-light" scope="col">{th}</th>)}
+                            {tableHead.map((th) => <th key={uniqueId()} className="text-light" scope="col">{th}</th>)}
                         </tr>
                     </thead>
                     <tbody>
                         {tableData ? tableData.map((detail) =>
-                            <tr class="table-light">
+                            <tr key={detail.id} className="table-light">
                                 <th scope="row">{detail.id}</th>
                                 <td>{detail.name}</td>
                                 <td>{detail.scheme}</td>
@@ -50,6 +51,8 @@ const tableHead = ["S.No", "Name of the Donar", "Donate to", "Amount"];
 const data = [{ id: 1, name: "Mark", scheme: "Bharatiya Suraj Bijili Yojana", amount: 5000 },
 { id: 2, name: "Mark", scheme: "Bharatiya Nari Shakti Yojana", amount: 10000 },
 { id: 3, name: "Mark", scheme: "Bharatiya Varshjal Sanchayan Yojana", amount: 10000 },
+{ id: 4, name: "Mark", scheme: "Bharatiya Nari Shakti Yojana", amount: 10000 },
+{ id: 5, name: "Mark", scheme: "Bharatiya Varshjal Sanchayan Yojana", amount: 10000 },
 ]
 
 export default DonarDetails;
